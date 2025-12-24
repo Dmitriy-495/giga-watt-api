f1a0cc75f23c1fcba4a51c8349426768ab44c1ca
Dmitriy-495 <tda495@yandex.ru>
2025-12-24 17:52:39 +0300

    LARAVEL SANCTUM FORTIFY

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/login', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/ping', function () {
+    return response()->json(['message' => 'API работает 🚀']);
+});
